f630c63c63cbb46b535e28cf52a77b5c8a92d99a
Kamil-87 <k.kamil87-87@yandex.ru>
2020-08-08 01:13:46 +0500

    start laravel

---
+++ resources/views/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная</title>
+</head>
+<body>
+    <?php include "menu.php"?>
+    <h1>Привет!</h1>
+</body>
+</html>
---
+++ resources/views/news.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Новости</title>
+</head>
+<body>
+    <?php include "menu.php"?>
+    <h1>Новости</h1>
+</body>
+</html>
